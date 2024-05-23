4ef1817e2f279db63fd5edd247ce3f11de1c11a5
gepinfo <info@geppettosoftware.com>
2024-05-23 07:19:10 +0000

    code commited from geppetto!

---
+++ application/services/default_services/systemcredentialmanager/src/credentials/securecredentials.ts
@@ -0,0 +1,4 @@
+export let credentials:any = {
+    DB_AUTH_studentsmanager_8089: 'mongodb://admin:password@',
+    DB_COLL_studentsmanager_8089: ':27017/studentsmanager_8089?authSource=admin'
+}
---
+++ devops/cloud/aws/github_actions/workflows/task-definition_0.ts
@@ -0,0 +1,85 @@
+module.exports = {
+    requiresCompatibilities: [
+        "FARGATE"
+    ],
+    "inferenceAccelerators": [],
+    "containerDefinitions": [
+        {
+            "name": "studmanager-8089",
+            "image": `${process.env.AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/geppetto-generatedcode-studentsmanager-8089-studmanager:latest`,
+            "memoryReservation": "300",
+            "resourceRequirements": null,
+            "essential": true,
+            "portMappings": [
+                {
+                    "containerPort": "8013",
+                    "protocol": "tcp",
+                    "hostPort": "8013"
+                }
+            ],
+            "environmentFiles": null,
+            "environment": [{"name":"MONGO_DB_URL","value":"mongodb://admin:password@studentsmanager.local:27017/studentsmanager_8089?authSource=admin"},{"name":"MONGO_DOMAIN","value":"studentsmanager.local"},{"name":"CAMUNDAPOD_URL","value":"http://studentsmanager.local:8080"},{"name":"SECURITYURL","value":"http://studentsmanager.local:8003"},{"name":"AUTHPROXYURL","value":"http://studentsmanager.local:8001"},{"name":"ADMINURL","value":"http://studentsmanager.local:8004"},{"name":"CAMUNDAURL","value":"http://studentsmanager.local:8002"},{"name":"GCAMURL","value":"http://studentsmanager.local:8007"},{"name":"APIGATEWAY","value":"http://studentsmanager.local:8000"}],
+            "secrets": null,
+            "mountPoints": [
+                {
+                  "readOnly": null,
+                  "containerPath": "/path/to/container",
+                  "sourceVolume": "studentsmanager"
+                }
+              ],
+            "volumesFrom": null,
+            "hostname": null,
+            "user": null,
+            "workingDirectory": null,
+            "extraHosts": null,
+            "logConfiguration": {
+                "logDriver": "awslogs",
+                "options": {
+                    "awslogs-group": "/ecs/studentsmanager",
+                    "awslogs-region": "us-east-1",
+                    "awslogs-stream-prefix": "ecs",
+                    "awslogs-create-group": "true"
+                }
+            },
+            "ulimits": null,
+            "dockerLabels": null,
+            "dependsOn": null,
+            "repositoryCredentials": {
+                "credentialsParameter": ""
+            }
+        },          
+    ],
+    "volumes": [
+        {
+        "fsxWindowsFileServerVolumeConfiguration": null,
+        "efsVolumeConfiguration": {
+            "transitEncryptionPort": null,
+            "fileSystemId": `${process.env.EFS_ID}`,
+            "authorizationConfig": {
+            "iam": "DISABLED",
+            "accessPointId": null
+            },
+            "transitEncryption": "DISABLED",
+            "rootDirectory": "/"
+        },
+        "name": "studentsmanager",
+        "host": null,
+        "dockerVolumeConfiguration": null
+        }
+    ],
+    "networkMode": "awsvpc",
+    "memory": "4096",
+    "cpu": "1024",
+    "executionRoleArn": `arn:aws:iam::${process.env.AWS_ACCOUNT_ID}:role/${process.env.AWS_ROLE_FOR_FARGATE}`,
+    "family": "studentsmanager0",
+    "tags": [],
+    "placementConstraints": [],
+    "taskRoleArn": `arn:aws:iam::${process.env.AWS_ACCOUNT_ID}:role/${process.env.AWS_ROLE_FOR_FARGATE}`
+
+};
+var ConfigurationJSON = JSON.stringify(module.exports);
+const config = module.exports;
+
+const jsonConfig = JSON.parse(JSON.stringify(ConfigurationJSON))
+
+console.log(jsonConfig)
